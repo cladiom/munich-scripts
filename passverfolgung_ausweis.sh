ac5b349ba1d3344fce62e3e5766ee0f63ad10a32
Cladio Martins <cladio.martins@bbv.eu>
2020-05-07 17:28:45 +0200

    Formats message

--- passverfolgung_ausweis.sh
+++ passverfolgung_ausweis.sh
@@ -1,5 +1,5 @@
 !/bin/bash
 
 if !(curl -d "ifNummer=XX08F9KMY4&pbAbfragen=Abfragen" https://www17.muenchen.de/Passverfolgung/ | grep "liegt" | xmllint --format --xpath '//TD/B[1]/text()' - | grep -q "nicht"); then
-    echo "Done" | mail -s "ID Card" you@mail.com
+    echo "is Done" | mail -s "ID Card" you@mail.com
 fi
