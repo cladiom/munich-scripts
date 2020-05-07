01e1c07071eb9248f30d8466e78612ba9fbf1703
Cladio Martins <cladio.martins@bbv.eu>
2020-05-07 17:26:41 +0200

    Removes personal data, oops

--- passverfolgung_ausweis.sh
+++ passverfolgung_ausweis.sh
@@ -1,7 +1,5 @@
 !/bin/bash
 
-if !(curl -d "ifNummer=LF08F9KMR5&pbAbfragen=Abfragen" https://www17.muenchen.de/Passverfolgung/ | grep "liegt" | xmllint --format --xpath '//TD/B[1]/text()' - | grep -q "nicht"); then
-    echo "Done" | mail -s "Personalausweis" juniormartins14@gmail.com
-else 
-    echo "In Progress" | mail -s "Personalausweis" juniormartins14@gmail.com
+if !(curl -d "ifNummer=XX08F9KMY4&pbAbfragen=Abfragen" https://www17.muenchen.de/Passverfolgung/ | grep "liegt" | xmllint --format --xpath '//TD/B[1]/text()' - | grep -q "nicht"); then
+    echo "Done" | mail -s "ID Card" you@mail.com
 fi
--- passverfolgung_reisepass.sh
+++ passverfolgung_reisepass.sh
@@ -1,7 +1,5 @@
 !/bin/bash
 
-if !(curl -d "ifNummer=CH1H7N48X4&pbAbfragen=Abfragen" https://www17.muenchen.de/Passverfolgung/ | grep "liegt" | xmllint --format --xpath '//TD/B[1]/text()' - | grep -q "nicht"); then
-    echo "Done" | mail -s "Reisepass" juniormartins14@gmail.com
-else 
-    echo "In Progress" | mail -s "Reisepass" juniormartins14@gmail.com
+if !(curl -d "ifNummer=XX1H7N48X3&pbAbfragen=Abfragen" https://www17.muenchen.de/Passverfolgung/ | grep "liegt" | xmllint --format --xpath '//TD/B[1]/text()' - | grep -q "nicht"); then
+    echo "is Done" | mail -s "Passport" you@mail.com
 fi
